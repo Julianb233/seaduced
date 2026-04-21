ed47c9cd99e4c34daf00a047238eaba5a46fd22c
Julian B <julianb233@gmail.com>
2026-04-21 01:24:46 -0700

    test: add test-showcase route for visual verification

---
+++ src/app/test-showcase/page.tsx
@@ -0,0 +1,9 @@
+import ProductShowcase from "../../components/sections/ProductShowcase";
+
+export default function TestShowcase() {
+  return (
+    <div className="min-h-screen bg-[#0A0A0A] flex items-center justify-center">
+      <ProductShowcase />
+    </div>
+  );
+}
